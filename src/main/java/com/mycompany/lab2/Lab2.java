6e6072845058f6e4cb741ffb11899f44d08f9b57
mrdrsn <nsokolova127@gmail.com>
2025-04-08 15:04:12 +0300

    first commit lab2

---
+++ src/main/java/com/mycompany/lab2/Lab2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab2;
+
+/**
+ *
+ * @author nsoko
+ */
+public class Lab2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
